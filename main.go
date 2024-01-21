3598457cc3ca18077ce687f838c2c93f24777550
saputhebeast <sapumalwijekoon4@gmail.com>
2024-01-21 17:38:38 +0530

    Feat: product operations

--- main.go
+++ main.go
@@ -19,6 +19,12 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api/users/:id", routes.GetUser)
 	app.Patch("/api/users/:id", routes.UpdateUser)
 	app.Delete("/api/users/:id", routes.DeleteUser)
+
+	app.Post("/api/products", routes.CreateProduct)
+	app.Get("/api/products", routes.GetProducts)
+	app.Get("/api/products/:id", routes.GetProduct)
+	app.Patch("/api/products/:id", routes.UpdateProduct)
+	app.Delete("/api/products/:id", routes.DeleteProduct)
 }
 
 func main() {
