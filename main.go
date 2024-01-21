3cceb472d9daf7c2f03680d807cb64267a54a13e
saputhebeast <sapumalwijekoon4@gmail.com>
2024-01-21 14:45:16 +0530

    Feat: user operations

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"go-fiber-api/database"
+	"go-fiber-api/routes"
 	"log"
 )
 
@@ -10,12 +11,22 @@ func welcome(c *fiber.Ctx) error {
 	return c.SendString("Welcome to my awesome API")
 }
 
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/health", welcome)
+
+	app.Post("/api/users", routes.CreateUser)
+	app.Get("/api/users", routes.GetUsers)
+	app.Get("/api/users/:id", routes.GetUser)
+	app.Patch("/api/users/:id", routes.UpdateUser)
+	app.Delete("/api/users/:id", routes.DeleteUser)
+}
+
 func main() {
 	database.ConnectionDb()
 
 	app := fiber.New()
 
-	app.Get("/v1", welcome)
+	setupRoutes(app)
 
 	log.Fatal(app.Listen(":3000"))
 }
